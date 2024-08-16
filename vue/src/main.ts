50ab95352186a4283e38a74b06dd566478054de4
Olivier Ribiere <o_ribiere@cartong.org>
2024-08-16 17:31:32 +0200

    init: Add vuetify & mapblibre

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -2,6 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +14,12 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+
+app.use(vuetify)
 
 app.mount('#app')
