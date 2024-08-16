5e4b7b093ccb511ef6f2027d5ca4da77fc346876
Olivier Ribiere <o_ribiere@cartong.org>
2024-08-16 18:12:09 +0200

    feat-Vue/Add vuetify theme

---
+++ vue/src/plugins/vuetify.ts
@@ -0,0 +1,26 @@
+
+import 'vuetify/styles'
+import { createVuetify, type ThemeDefinition }  from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const pucCustomTheme: ThemeDefinition = {
+    colors: {
+      'main-blue': '#1c3b87',
+      'main-red': '#E83323',
+      'main-yellow': '#F6CC47',
+      'main-grey': "#E0E0E0",
+      'main-green': "#2D6438"
+    },
+  }
+
+export const vuetify = createVuetify({
+    theme: {
+        defaultTheme: 'pucCustomTheme',
+        themes: {
+            pucCustomTheme,
+        },
+      },
+    components,
+    directives,
+  })
--- vue/src/views/ActorsView.vue
+++ vue/src/views/ActorsView.vue
@@ -1,5 +1,6 @@
 <template>
     <div>I'm the actors component</div>
+    <v-btn color="main-red">Button</v-btn>
 </template>
 <script setup lang="ts">
 </script>
