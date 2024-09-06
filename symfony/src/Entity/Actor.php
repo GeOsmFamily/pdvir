8b192f94ffe18bcd41fca7cd35465aea1557e582
Olivier Ribiere <o_ribiere@cartong.org>
2024-09-06 14:25:42 +0200

    temp(sf): Vaine tentative de connexion

---
+++ symfony/src/Entity/Actor.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Post;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ActorRepository;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+
+#[ORM\Entity(repositoryClass: ActorRepository::class)]
+#[ApiResource]
+#[Get]
+#[Put(security: "is_granted('ROLE_EDITOR') or object.owner == user")]
+#[GetCollection]
+#[Post(security: "is_granted('ROLE_EDITOR')")]
+class Actor
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $acronym = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAcronym(): ?string
+    {
+        return $this->acronym;
+    }
+
+    public function setAcronym(string $acronym): static
+    {
+        $this->acronym = $acronym;
+
+        return $this;
+    }
+}
