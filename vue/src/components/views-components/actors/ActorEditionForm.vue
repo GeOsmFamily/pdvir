4ad604e1bf4e44ab6defa1a33059026cf881f9bb
Olivier Ribiere <o_ribiere@cartong.org>
2024-09-09 19:23:00 +0200

    Feat(vue): Add image input in actor form

--- vue/src/components/views-components/actors/ActorEditionForm.vue
+++ vue/src/components/views-components/actors/ActorEditionForm.vue
@@ -9,7 +9,7 @@
             <span class="ml-2">Nouvelle soumission de Prénom NOM reçue le 31 janvier 2025 à 11h30.</span>
         </div>
         <div class="Actors__form__container mt-4">
-            <v-form @submit.prevent="plop">             
+            <v-form @submit.prevent="submitForm">             
                 <v-text-field
                     density="compact"
                     variant="outlined"
@@ -56,22 +56,53 @@
                     type="tel"
                     :label="$t('actors.form.phone')"
                 ></v-text-field>
+
+            <div
+                class="Actors__form__dropzone"
+                :class="{ 'Actors__form__dropzone--dragging': isDragging }"
+                @drop.prevent="handleDrop"
+                @dragenter.prevent="handleDragEnter"
+                @dragover.prevent="handleDragOver"
+                @dragleave.prevent="handleDragLeave"
+                @click="triggerFileInput"
+            >
+                <img src="@/assets/images/importFilesIcon.svg" alt="">
+                <p class="ml-2">
+                    {{$t('actors.form.drag')}}
+                    <span class="Actors__form__dropzone__uploadLink">{{$t('actors.form.import')}}</span></p>
+                <input
+                    ref="fileInput"
+                    type="file"
+                    multiple
+                    accept="image/*"
+                    class="d-none"
+                    @change="handleFileChange"
+                />
+            </div>
+
+            <div v-if="selectedFiles.length" class="d-flex flex-wrap mt-3">
+                <div v-for="(file, index) in selectedFiles":key="index" class="position-relative">
+                    <div @click="removeFile(index)" class="Actors__form__dropzone__imageCloser">X</div>
+                    <img
+                        :src="file.preview"
+                        :alt="file.name"
+                        class="Actors__form__dropzone__imageLoaded ma-2"
+                    />
+                </div>
+            </div>
+                
+
+
                 <div class="d-flex justify-space-between">
+                    <v-btn color="white">Supprimer</v-btn>
                     <v-btn 
-                        text="Supprimer"
-                        color="white"
-                    ></v-btn>
-                    <v-btn 
-                        text="Valider"
                         type="submit"
                         color="main-red"
                         :loading="isSubmitting"
-                    ></v-btn>
+                    >Valider</v-btn>
                 </div>
-                
             </v-form>
         </div>
-        
     </div>
     
 </template>
@@ -81,22 +112,48 @@ import { ActorsFormService } from '@/services/actors/ActorsForm';
 import { useActorsStore } from '@/stores/actorsStore';
 import { useApplicationStore } from '@/stores/applicationStore';
 import FormSectionTitle from '@/components/generic-components/text-elements/FormSectionTitle.vue';
+import { ref } from 'vue';
 const appStore = useApplicationStore();
 const actorsStore = useActorsStore();
 const actorToEdit: Actor | undefined = actorsStore.actors.find(actor => actor.id === actorsStore.actorEdition.id);
-
 const {form, errors, handleSubmit, isSubmitting} = ActorsFormService.getActorsForm(actorToEdit);
 
-const plop = handleSubmit((values) => {
+const selectedFiles: any = ref([])
+const fileInput = ref(null)
+
+const triggerFileInput = () => {
+  (fileInput.value as any).click()
+}
+const handleDrop = (event: any) => {
+  const files = event.dataTransfer.files
+  handleFileChange({ target: { files } })
+}
+
+const handleFileChange = (event: any) => {
+  const files = event.target.files
+  Array.from(files).forEach(file => {
+    const preview = URL.createObjectURL(file as File)
+    selectedFiles.value.push({ name: (file as File).name, preview, file })
+  })
+}
+const removeFile = (index: number) => {
+    selectedFiles.value.splice(index, 1)
+}
+
+const isDragging = ref(false)
+const handleDragEnter = () => {
+  isDragging.value = true
+}
+const handleDragOver = (event: Event) => {
+  event.preventDefault()
+}
+const handleDragLeave = (event: any) => {
+    if (!event.currentTarget.contains(event.relatedTarget)) {
+        isDragging.value = false
+    }
+}
+
+const submitForm = handleSubmit((values) => {
     console.log(values)
 })
-// const plop = () => {
-//     console.log(submitForm)
-//     if (Object.keys(errors.value).length === 0) {
-//         console.log({...actorToEdit,...form})
-//         console.log("let's go")
-//     } else {
-//         console.log("let's not go")
-//     }
-// };
 </script>
