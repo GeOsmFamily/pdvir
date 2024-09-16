dd6301ff0f5b0406b111f32b59bc22fa60a6974c
Pierre-Louis <pl_duthoit@cartong.org>
2024-09-16 16:10:51 +0200

    init(sentry): Add sentry to symfony, and use generic SENTRY_DSN root env var

--- vue/src/assets/plugins/sentry.ts
+++ vue/src/assets/plugins/sentry.ts
@@ -3,15 +3,17 @@ import type { App } from "vue";
 import type { Router } from "vue-router";
 
 export const initSentry = (app: App, router: Router) => {
-  Sentry.init({
-    app,
-    dsn: "https://e9bd6bf2e6b703942957b86a4816bf97@o1427569.ingest.us.sentry.io/4507946046259200",
-    integrations: [
-      Sentry.browserTracingIntegration({ router }),
-    ],
-    // Tracing
-    tracesSampleRate: 1.0, //  Capture 100% of the transactions
-    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-    tracePropagationTargets: [/^https:\/\/(dev|uat)\.puc.\.cartong\.org/],
-  });
+  if (import.meta.env.MODE === "production") {
+    Sentry.init({
+      app,
+      dsn: import.meta.env.VITE_SENTRY_DSN,
+      integrations: [
+        Sentry.browserTracingIntegration({ router }),
+      ],
+      // Tracing
+      tracesSampleRate: 1.0, //  Capture 100% of the transactions
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: [/^https:\/\/(dev|uat)\.puc.\.cartong\.org/],
+    });
+  }
 }
