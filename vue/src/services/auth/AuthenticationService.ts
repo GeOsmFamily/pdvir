78048395b12a85dc6262a539a01256e28bdc90af
plduthoit <139240078+plduthoit@users.noreply.github.com>
2024-10-08 11:13:01 +0200

    Update AuthenticationService.ts

--- vue/src/services/auth/AuthenticationService.ts
+++ vue/src/services/auth/AuthenticationService.ts
@@ -1,24 +1,25 @@
 import type { SignInValues, SignUpValues } from "@/models/interfaces/auth/AuthenticationsValues";
 import type { User } from "@/models/interfaces/auth/User";
-import axios, { type AxiosResponse } from "axios";
+import { apiClient } from '@/assets/plugins/axios'
+import type { AxiosResponse } from "axios";
 
 
 export class AuthenticationService {
     static async signIn(values: SignInValues): Promise<AxiosResponse> {
-        return axios.post('https://puc.local/auth', JSON.stringify(values), {
+        return apiClient.post('/auth', JSON.stringify(values), {
             headers: { 'Content-Type': 'application/json' }
         })
     }
 
     static async signUp(values: SignUpValues): Promise<AxiosResponse> {
-        return axios.post('https://puc.local/api/users', JSON.stringify(values), { headers: { 'Content-Type': 'application/ld+json' } })
+        return apiClient.post('/api/users', JSON.stringify(values), { headers: { 'Content-Type': 'application/ld+json' } })
     }
 
     static async getAuthenticatedUser(): Promise<AxiosResponse<User>> {
-        return axios.get('https://puc.local/api/users/me', { headers:  {'accept': 'application/ld+json'}})
+        return apiClient.get('/api/users/me', { headers:  {'accept': 'application/ld+json'}})
     }
 
     static async patchUser(values: Partial<User>, userID: number): Promise<AxiosResponse> {
-        return axios.patch(`https://puc.local/api/users/${userID}`, JSON.stringify(values), { headers: { 'Content-Type': 'application/merge-patch+json' } })
+        return apiClient.patch(`/api/users/${userID}`, JSON.stringify(values), { headers: { 'Content-Type': 'application/merge-patch+json' } })
     }
 }
