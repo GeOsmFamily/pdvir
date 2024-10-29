8bff7aba41ce38ace54ce2bf97e5f10046ef7802
Olivier Ribiere <o_ribiere@cartong.org>
2024-10-29 14:31:15 +0100

    feat(account): Update avatar and translations

--- vue/src/views/member/MemberView.vue
+++ vue/src/views/member/MemberView.vue
@@ -4,11 +4,16 @@
         <div class="UserBlock UserBlock--left">
             <div class="UserAccount__avatarBlock">
                 <img src="@/assets/images/user/default_avatar.png" alt="">
-                <div class="d-flex flex-column flex-grow-1 ml-8">
+                <div class="d-flex flex-column ml-8 w-100">
                     <v-text-field :label="$t('auth.becomeMember.form.firstName')" />
                     <v-text-field :label="$t('auth.becomeMember.form.lastName')" />
                 </div>
             </div>
+            <v-text-field :label="$t('auth.becomeMember.form.email')" />
+            <v-text-field :label="$t('auth.becomeMemberThanks.form.organization')" />
+            <v-text-field :label="$t('auth.becomeMemberThanks.form.functions')" />
+            <v-text-field :label="$t('auth.becomeMemberThanks.form.telephone')" />
+            <a href="#">{{ $t('account.changePassword')}}</a>
         </div>
     </div>
 </template>
@@ -46,9 +51,15 @@ import SectionBanner from '@/components/banners/SectionBanner.vue';
         flex-direction: row;
         img {
             height: 8rem;
+            cursor: pointer;
         }
         @media (max-width: $bp-sm){
             flex-direction: column;
+            align-items: center;
+            img {
+                width: 8rem;
+                margin-bottom: 1rem;
+            }
         }
     }
 }
