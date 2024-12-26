796e51a6a33517c444a2e95cce91eae02f3f08dc
plduthoit <plduthoit@users.noreply.github.com>
2024-12-26 13:15:04 +0000

    Fix styling

--- symfony/src/Controller/Qgis/QgisController.php
+++ symfony/src/Controller/Qgis/QgisController.php
@@ -8,44 +8,43 @@ use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class QgisController extends AbstractController
 {
-  public function __construct(
-    private HttpClientInterface $httpClient,
-    private Security $security,
-    private ParameterBagInterface $params
-  ) {}
-
-  #[Route('/api/qgis/{path}', requirements: ['path' => '.*'], methods: ['GET'], name: 'qgis')]
-  public function proxy(
-    string $path,
-    RequestStack $requestStack
-  ): Response
-  {
-    
-    // Add any user permission checks 
-    $this->denyAccessUnlessGranted('ROLE_USER');
-
-    $queryParams = $requestStack->getCurrentRequest()->query->all();
-    $qgisUrl = 'https://qgis.' . $this->params->get('domain') . '/' . $path;
-
-    try {
-      // Forward the request to QGIS Server
-      $qgisResponse = $this->httpClient->request('GET', $qgisUrl, [
-        'query' => $queryParams
-      ]);
-
-      // Return the QGIS response
-      return new Response(
-        $qgisResponse->getContent(),
-        $qgisResponse->getStatusCode(),
-        $qgisResponse->getHeaders(false)
-      );
-    } catch (\Exception $e) {
-      return $this->json(['error' => 'Failed to fetch data from QGIS Server', 'details' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+    public function __construct(
+        private HttpClientInterface $httpClient,
+        private Security $security,
+        private ParameterBagInterface $params,
+    ) {
     }
-  }
+
+    #[Route('/api/qgis/{path}', requirements: ['path' => '.*'], methods: ['GET'], name: 'qgis')]
+    public function proxy(
+        string $path,
+        RequestStack $requestStack,
+    ): Response {
+        // Add any user permission checks
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $queryParams = $requestStack->getCurrentRequest()->query->all();
+        $qgisUrl = 'https://qgis.'.$this->params->get('domain').'/'.$path;
+
+        try {
+            // Forward the request to QGIS Server
+            $qgisResponse = $this->httpClient->request('GET', $qgisUrl, [
+                'query' => $queryParams,
+            ]);
+
+            // Return the QGIS response
+            return new Response(
+                $qgisResponse->getContent(),
+                $qgisResponse->getStatusCode(),
+                $qgisResponse->getHeaders(false)
+            );
+        } catch (\Exception $e) {
+            return $this->json(['error' => 'Failed to fetch data from QGIS Server', 'details' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
