8192bd4f37f5ecf7c762bfbb44b14f1e48f3e25d
Olivier Ribiere <o_ribiere@cartong.org>
2025-02-10 15:16:14 +0100

    test base64

--- vue/src/services/utils/UtilsService.ts
+++ vue/src/services/utils/UtilsService.ts
@@ -90,6 +90,7 @@ export function downloadJson(data: any, fileName: string) {
 }
 
 export async function fetchImageAsBase64(url: string): Promise<string | null> {
+  console.log(url)
   try {
     const response = await fetch(url)
     if (!response.ok) {
