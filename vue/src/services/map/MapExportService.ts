3c9c6db92fddddc955563305f71edc46d12dc70b
Olivier Ribiere <o_ribiere@cartong.org>
2025-02-12 11:56:41 +0100

    feat(pdf): Set description as optionnal

--- vue/src/services/map/MapExportService.ts
+++ vue/src/services/map/MapExportService.ts
@@ -1,16 +1,13 @@
 import { z } from 'zod'
-import { CommonZodSchema } from '../forms/CommonZodSchema'
 import { i18n } from '@/plugins/i18n'
 import { toTypedSchema } from '@vee-validate/zod'
 import { useForm, useField } from 'vee-validate'
 
 export class MapExportService {
   static getExportForm() {
-    const zodModels = CommonZodSchema.getDefinitions()
     const formSchema = z.object({
-      ///////// Main infos \\\\\\\\\
       title: z.string().min(1, { message: i18n.t('forms.errorMessages.required') }),
-      description: zodModels.description
+      description: z.string().optional()
     })
     const { errors, handleSubmit, isSubmitting } = useForm({
       validationSchema: toTypedSchema(formSchema)
