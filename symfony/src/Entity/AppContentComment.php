9a2854470c4694bc99f0227700edb2ad88895941
Olivier Ribiere <o_ribiere@cartong.org>
2025-02-13 17:21:44 +0100

    feat(sf): Add app content comment entity

---
+++ symfony/src/Entity/AppContentComment.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
+use Doctrine\DBAL\Types\Types;
+use ApiPlatform\Metadata\Delete;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use App\Entity\Trait\BlameableEntity;
+use ApiPlatform\Metadata\GetCollection;
+use App\Entity\Trait\TimestampableEntity;
+use App\Repository\AppContentCommentRepository;
+
+#[ORM\Entity(repositoryClass: AppContentCommentRepository::class)]
+#[ApiResource(
+    operations: [
+        new GetCollection(security: 'is_granted("ROLE_ADMIN")'),
+        new Post(),
+        new Delete(
+            security: 'is_granted("ROLE_ADMIN")'
+        ),
+    ]
+)]
+class AppContentComment
+{
+    use TimestampableEntity;
+    use BlameableEntity;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?bool $readByAdmin = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $message = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function isReadByAdmin(): ?bool
+    {
+        return $this->readByAdmin;
+    }
+
+    public function setReadByAdmin(bool $readByAdmin): static
+    {
+        $this->readByAdmin = $readByAdmin;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): static
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
