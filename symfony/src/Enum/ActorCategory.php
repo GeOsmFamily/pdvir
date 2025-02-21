66b2abbe518c665a8cfad7787c2e37b0fe8f2b01
Igornovic21 <kamguefokoigor@gmail.com>
2025-02-21 12:16:06 +0100

    feat(actor): update category list

--- symfony/src/Enum/ActorCategory.php
+++ symfony/src/Enum/ActorCategory.php
@@ -7,10 +7,11 @@ use App\Enum\Trait\ToArray;
 enum ActorCategory: string
 {
     use ToArray;
+    case INTERNATIONAL = 'International';
     case INSTITUTIONAL_ACTORS = 'Acteurs institutionnels';
     case COMPANIES_PUBLIC_ENTITIES = 'Entreprises et établissements publics';
     case TRAINING_RESEARCH_DATA_STRUCTURES = 'Structures de formation, recherche et production de données';
     case NON_INSTITUTIONAL_ACTORS = 'Acteurs non institutionnels';
-    case BILATERAL_MULTILATERAL_PARTNERS = 'Partenaires bilatèreux et multilatèreux';
+    case BILATERAL_MULTILATERAL_PARTNERS = 'Partenaires bilatéraux et multilatéraux';
     case CONSULTATION_SPACES = 'Espaces de concertation';
 }
