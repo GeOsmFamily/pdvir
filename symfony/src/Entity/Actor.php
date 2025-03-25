20f5f83c419a76be1b3644c87e02a35acb0d13a0
plduthoit <plduthoit@users.noreply.github.com>
2025-03-25 11:26:38 +0000

    Fix styling

--- symfony/src/Entity/Actor.php
+++ symfony/src/Entity/Actor.php
@@ -26,7 +26,6 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Jsor\Doctrine\PostGIS\Types\PostGISType;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Uid\Uuid;
