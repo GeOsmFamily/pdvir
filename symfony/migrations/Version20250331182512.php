724ecc98475f108b5e69a8242d613c8a0483154b
Olivier Ribiere <o_ribiere@cartong.org>
2025-04-04 18:15:59 +0200

    fix(migrations): add ids to insert commands

--- symfony/migrations/Version20250331182512.php
+++ symfony/migrations/Version20250331182512.php
@@ -22,22 +22,27 @@ final class Version20250331182512 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE resource ADD other_type VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE resource ADD other_thematic VARCHAR(255) DEFAULT NULL');
-        $this->addSql("INSERT INTO actor_expertise (name) VALUES ('Autre')");
-        $this->addSql("delete from organisation where name LIKE 'Deutsche Gesellschaft für Internationale Zusammenarbeit'");
-        $this->addSql("delete from organisation where name LIKE 'Association Humanitaire de Solidarité Internationale'");
-        $this->addSql("delete from organisation where name LIKE 'ONU-Habitat'");
-        $this->addSql("delete from organisation where name LIKE 'Bundesministerium für wirtschaftliche Zusammenarbeit und Entwicklung'");
-        $this->addSql("delete from organisation where name LIKE 'Crédit Foncier du Cameroun'");
-        $this->addSql("delete from organisation where name LIKE 'Union Européenne'");
-        $this->addSql("insert into organisation (name, acronym, contracting, donor) values ('Autre', 'Others', true, true)");
-        $this->addSql("insert into organisation (name, acronym, contracting, donor) values ('Fonds propres', 'FP', true, true)");
-        $this->addSql("insert into organisation (name, acronym, contracting, donor) values ('Subvention publique', 'SP', true, true)");
-        $this->addSql("insert into organisation (name, acronym, contracting, donor) values ('Prêt bancaire', 'PB', true, true)");
-        $this->addSql("insert into organisation (name, acronym, contracting, donor) values ('Investissement privé', 'IP', true, true)");
-        $this->addSql("insert into organisation (name, acronym, contracting, donor) values ('Crowdfunding (financement participatif)', 'CF', true, true)");
-        $this->addSql("insert into organisation (name, acronym, contracting, donor) values ('Partenariat public-privé', 'PPP', true, true)");
-        $this->addSql("insert into organisation (name, acronym, contracting, donor) values ('Mécénat', 'MC', true, true)");
-        $this->addSql("insert into thematic (name) values ('Autre')");
+        
+
+        $this->addSql("DELETE FROM organisation WHERE name LIKE 'Deutsche Gesellschaft für Internationale Zusammenarbeit'");
+        $this->addSql("DELETE FROM organisation WHERE name LIKE 'Association Humanitaire de Solidarité Internationale'");
+        $this->addSql("DELETE FROM organisation WHERE name LIKE 'ONU-Habitat'");
+        $this->addSql("DELETE FROM organisation WHERE name LIKE 'Bundesministerium für wirtschaftliche Zusammenarbeit und Entwicklung'");
+        $this->addSql("DELETE FROM organisation WHERE name LIKE 'Crédit Foncier du Cameroun'");
+        $this->addSql("DELETE FROM organisation WHERE name LIKE 'Union Européenne'");
+
+        $this->addSql("INSERT INTO actor_expertise (id, name) VALUES (nextval('actor_expertise_id_seq'), 'Autre')");
+        
+        $this->addSql("INSERT INTO organisation (id, name, acronym, contracting, donor) VALUES (nextval('organisation_id_seq'), 'Autre', 'Others', true, true)");
+        $this->addSql("INSERT INTO organisation (id, name, acronym, contracting, donor) VALUES (nextval('organisation_id_seq'), 'Fonds propres', 'FP', true, true)");
+        $this->addSql("INSERT INTO organisation (id, name, acronym, contracting, donor) VALUES (nextval('organisation_id_seq'), 'Subvention publique', 'SP', true, true)");
+        $this->addSql("INSERT INTO organisation (id, name, acronym, contracting, donor) VALUES (nextval('organisation_id_seq'), 'Prêt bancaire', 'PB', true, true)");
+        $this->addSql("INSERT INTO organisation (id, name, acronym, contracting, donor) VALUES (nextval('organisation_id_seq'), 'Investissement privé', 'IP', true, true)");
+        $this->addSql("INSERT INTO organisation (id, name, acronym, contracting, donor) VALUES (nextval('organisation_id_seq'), 'Crowdfunding (financement participatif)', 'CF', true, true)");
+        $this->addSql("INSERT INTO organisation (id, name, acronym, contracting, donor) VALUES (nextval('organisation_id_seq'), 'Partenariat public-privé', 'PPP', true, true)");
+        $this->addSql("INSERT INTO organisation (id, name, acronym, contracting, donor) VALUES (nextval('organisation_id_seq'), 'Mécénat', 'MC', true, true)");
+
+        $this->addSql("INSERT INTO thematic (id, name) VALUES (nextval('thematic_id_seq'), 'Autre')");
     }
 
     public function down(Schema $schema): void
