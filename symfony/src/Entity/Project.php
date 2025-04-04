4333b361fe3e4481e7a742162f795f5fd5de7176
plduthoit <plduthoit@users.noreply.github.com>
2025-04-04 12:02:47 +0000

    Fix styling

--- symfony/src/Entity/Project.php
+++ symfony/src/Entity/Project.php
@@ -12,7 +12,6 @@ use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
-use ApiPlatform\Metadata\QueryParameter;
 use App\Controller\Project\SimilarProjectsAction;
 use App\Entity\File\MediaObject;
 use App\Entity\Trait\BlameableEntity;
