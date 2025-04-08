db53c35777f242ae6691fe479dbbf0e28d4a2f97
Olivier Ribiere <o_ribiere@cartong.org>
2025-04-08 17:34:42 +0200

    fix(actors): set localisation as nullable

--- vue/src/services/actors/ActorsForm.ts
+++ vue/src/services/actors/ActorsForm.ts
@@ -29,7 +29,7 @@ export class ActorsFormService {
       ///////// Contact \\\\\\\\\
       officeName: z.string().optional(),
       officeAddress: z.string().optional(),
-      geoData: zodModels.geoData,
+      geoData: zodModels.geoDataNullable.optional(),
       contactName: z.string().optional(),
       contactPosition: z.string().optional(),
       website: zodModels.website,
